8bf6a55bd20efc319d59458a3f4ca3d87b900596
Koen Betsens <koen.betsens@gmail.com>
2015-09-13 22:31:40 +0200

    Some more header updates

--- web/app/themes/damn-sage/templates/header-magazine.php
+++ web/app/themes/damn-sage/templates/header-magazine.php
@@ -5,22 +5,9 @@
  */
 global $issue, $issue_color, $issue_number;
 
-$issue = $_GET['issue']?
-
-	get_term_by('slug', preg_replace ("/[^A-Za-z0-9-]/", '', $_GET['issue']), 'magazine'):
-	get_field ('current_issue', 'option');
-
-
-if (!$issue) $issue = get_field ('current_issue', 'option');
-if (!$issue)
-
-	 throw new Exception('No current issue is set, please contact the DAMn° Moderator.');
-
 // Some dry data
 $issue_acf_id = 'magazine_' . $issue->term_id;
 $contrast = (int) get_field ('colour_scheme', $issue_acf_id);
-$issue_color = get_field ('issue_color', $issue_acf_id);
-$issue_number = get_field ('magazine_number', $issue_acf_id);
 $issue_link = get_term_link($issue->term_id, 'magazine');
 $header_image = get_field ('header_image', $issue_acf_id);
 $issue_cat = get_field ('primary_category', $issue_acf_id);
--- web/app/themes/damn-sage/templates/header.php
+++ web/app/themes/damn-sage/templates/header.php
@@ -1,8 +1,30 @@
 <?php
-	// This file assumes that you have included the nav walker from https://github.com/twittem/wp-bootstrap-navwalker
-	// somewhere in your theme.
-	
-	// Identifier between issue- and event-based header will be placed here
+// This file assumes that you have included the nav walker from https://github.com/twittem/wp-bootstrap-navwalker
+// somewhere in your theme.
+
+// Identifier between issue- and event-based header will be placed here
+
+
+/**
+ *	Selected Issue
+ */
+global $issue, $issue_color, $issue_number;
+
+$issue = $_GET['issue']?
+
+	get_term_by('slug', preg_replace ("/[^A-Za-z0-9-]/", '', $_GET['issue']), 'magazine'):
+	get_field ('current_issue', 'option');
+
+
+if (!$issue) $issue = get_field ('current_issue', 'option');
+if (!$issue)
+
+	 throw new Exception('No current issue is set, please contact the DAMn° Moderator.');
+
+// Some dry data
+$issue_acf_id = 'magazine_' . $issue->term_id;
+$issue_color = get_field ('issue_color', $issue_acf_id);
+$issue_number = get_field ('magazine_number', $issue_acf_id);
 ?>
 
 <?php if (is_home()): ?>
@@ -10,31 +32,7 @@
 	<?php get_template_part('templates/header', 'magazine'); // or 'event' ?>
 	
 <?php else: ?>
-	
-	<?php	
-	// Big Fat !Hack!
-	// non-dry data from templates/header-magazine
-	// Until decided on non-home header
-	global $issue, $issue_color, $issue_number;
-	
-	$issue = $_GET['issue']?
-	
-		get_term_by('slug', preg_replace ("/[^A-Za-z0-9-]/", '', $_GET['issue']), 'magazine'):
-		get_field ('current_issue', 'option');
-	
-	
-	if (!$issue) $issue = get_field ('current_issue', 'option');
-	if (!$issue)
-	
-		 throw new Exception('No current issue is set, please contact the DAMn° Moderator.');
-	
-	// Some dry data
-	$issue_acf_id = 'magazine_' . $issue->term_id;
-	$issue_color = get_field ('issue_color', $issue_acf_id);
-	$issue_number = get_field ('magazine_number', $issue_acf_id);
-	?>
-	
-	
+
 	<div class="home-feature">
 		<?php get_template_part('templates/snippet', 'header-nav'); ?>
 	</div>
