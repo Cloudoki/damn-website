de61cf97a24cd2a91470bdc460309df956dcee30
Michael Lythcott <mike@reccenter.com>
2015-09-13 19:30:20 +0100

    updated colophon with dynamic author list

--- web/app/themes/damn-sage/page-colophon.php
+++ web/app/themes/damn-sage/page-colophon.php
@@ -18,6 +18,37 @@ Template Name: Colophon
 
     <div class="col-xs-12 col-ms-6 col-sm-6">
       <div class="col-xs-12 col-md-6 column-1">
+
+        <?php
+        // Get all users order by amount of posts
+        $allUsers = get_users('orderby=post_count&order=DESC&showposts=15');
+        $users = array();
+
+        // Remove subscribers from the list as they won't write any articles
+        foreach($allUsers as $currentUser) {
+          if(!in_array( 'subscriber', $currentUser->roles )) {
+            $users[] = $currentUser;
+          }
+        }
+        ?>
+
+        <h3 class="text-uppercase">
+          Contributors
+        </h3>
+
+        <div class="contributor-names">
+          <?php foreach($users as $user) { ?>
+            <span>
+              <a href="<?php echo get_author_posts_url( $user->ID ); ?>" title="Read Articles" class="black-link">
+                <?php echo $user->display_name; ?>
+              </a>
+            </span>
+          <?php } ?>
+          <a href="/colophon/contributors/" title="All Contributors" class="btn btn-primary marginTop marginBottom15">All Contributors</a>
+        </div>
+
+
+
         <?php if(get_field('column_1')) { ?>
           <?php the_field('column_1'); ?>
         <?php } ?>
