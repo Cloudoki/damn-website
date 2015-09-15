a0501a8b2fcc468df11da84fc8580336f5e3e5ce
Michael Lythcott <mike@reccenter.com>
2015-09-15 12:49:01 +0100

    updated style of join confirmation page

--- web/app/themes/damn-sage/paypal-return.php
+++ web/app/themes/damn-sage/paypal-return.php
@@ -17,49 +17,43 @@
         <main class="main " role="main">
 
           <div class="row">
+            <div class="col-xs-12 col-sm-6 col-sm-offset-3 text-center">
+              <div style="border: 1px solid #e5e5e5; padding: 20px;text-align: center;">
+                <img src="/app/uploads/2015/09/damnplus-black.png" alt="DAMN+">
 
-            <img src="/app/uploads/2015/09/damnplus-black.png">
-            <hr />
+                <?php
+                if(!defined('WPINC')) // MUST have WordPress.
+                  exit("Do not access this file directly.");
+                ?>
 
-            <?php
-            if(!defined('WPINC')) // MUST have WordPress.
-              exit("Do not access this file directly.");
-            ?>
-
-            <!-- Header Section (contains information and possible custom code from the originating site/domain). -->
-            <div id="s2member-default-return-header-section" class="s2member-return-section s2member-return-header-section s2member-default-return-header-section">
-              <div id="s2member-default-return-header-div" class="s2member-return-div s2member-return-header-div s2member-default-return-header-div">
-                %%header%% <!-- (this is auto-filled by s2Member, based on configuration). -->
-              </div>
-              <div style="clear:both;"></div>
-            </div>
-
-            <!-- Response Section (this is auto-filled by s2Member, based on what action has taken place). -->
-            <!-- Although NOT recommended, you can remove the response Replacement Code and build your own message if you prefer. -->
-            <!-- It is NOT recommended, because the dynamic response message may vary, depending on what action has taken place. -->
-            <div id="s2member-default-return-response-section" class="s2member-return-section s2member-return-response-section s2member-default-return-response-section">
-              <div id="s2member-default-return-response-div" class="s2member-return-div s2member-return-response-div s2member-default-return-response-div">
-                %%response%% <!-- (this is auto-filled by s2Member, based on what action has taken place). -->
-                <div id="s2member-default-return-continue" class="s2member-return-continue s2member-default-return-continue">
-                  %%continue%% <!-- (auto-filled by s2Member, based on what action has taken place). -->
-                  <br /><br />
-                  <a href="/wp/wp-login.php?action=register" class="btn btn-primary btn-lg" title="REGISTER NOW">REGISTER NOW</a>
+                <!-- Response Section (this is auto-filled by s2Member, based on what action has taken place). -->
+                <!-- Although NOT recommended, you can remove the response Replacement Code and build your own message if you prefer. -->
+                <!-- It is NOT recommended, because the dynamic response message may vary, depending on what action has taken place. -->
+                <div id="s2member-default-return-response-section" class="s2member-return-section s2member-return-response-section s2member-default-return-response-section">
+                  <div id="s2member-default-return-response-div" class="s2member-return-div s2member-return-response-div s2member-default-return-response-div">
+                    %%response%% <!-- (this is auto-filled by s2Member, based on what action has taken place). -->
+                    <div id="s2member-default-return-continue" class="s2member-return-continue s2member-default-return-continue">
+                      %%continue%% <!-- (auto-filled by s2Member, based on what action has taken place). -->
+                      <br /><br />
+                      <a href="/wp/wp-login.php?action=register" class="btn btn-primary btn-lg marginBottom" title="REGISTER NOW">REGISTER NOW</a>
+                    </div>
+                  </div>
+                  <div style="clear:both;"></div>
                 </div>
+
+                <!-- Support Section (contains information about how a Customer can contact support). -->
+                <div id="s2member-default-return-support-section" class="s2member-return-section s2member-return-support-section s2member-default-return-support-section">
+                  <div id="s2member-default-return-support-div" class="s2member-return-div s2member-return-support-div s2member-default-return-support-div">
+                    %%support%% <!-- (this is auto-filled by s2Member, based on configuration). -->
+                    If you need assistance, <a href="/join-damn-plus/assistance">go here</a>.
+                  </div>
+                  <div style="clear:both;"></div>
+                </div>
+
+                %%tracking%% <!-- (this is auto-filled, supports tracking codes integrated w/ s2Member). -->
+                <div style="clear:both;"></div>
               </div>
-              <div style="clear:both;"></div>
             </div>
-
-            <!-- Support Section (contains information about how a Customer can contact support). -->
-            <div id="s2member-default-return-support-section" class="s2member-return-section s2member-return-support-section s2member-default-return-support-section">
-              <div id="s2member-default-return-support-div" class="s2member-return-div s2member-return-support-div s2member-default-return-support-div">
-                %%support%% <!-- (this is auto-filled by s2Member, based on configuration). -->
-                If you need assistance, <a href="/join-damn-plus/assisstance">go here</a>.
-              </div>
-              <div style="clear:both;"></div>
-            </div>
-
-            %%tracking%% <!-- (this is auto-filled, supports tracking codes integrated w/ s2Member). -->
-
           </div>
 
         </main><!-- /.main -->
