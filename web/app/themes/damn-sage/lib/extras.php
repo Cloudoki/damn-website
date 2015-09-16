283749dcde1bcf2e39c8a1e748b43e45a4691b37
Michael Lythcott <mike@reccenter.com>
2015-09-16 13:29:22 +0100

    updated auto email clickable code

--- web/app/themes/damn-sage/lib/extras.php
+++ web/app/themes/damn-sage/lib/extras.php
@@ -43,11 +43,8 @@ function custom_excerpt_length($length) {
 }
 add_filter( 'excerpt_length', __NAMESPACE__ . '\\custom_excerpt_length', 999 );
 
-
-// make links and email addresses clickable
-
-add_filter('the_content', __NAMESPACE__ . '\\make_clickable');
-add_filter('the_excerpt', __NAMESPACE__ . '\\make_clickable');
+add_filter('the_content', 'make_clickable');
+add_filter('the_excerpt', 'make_clickable');
 
 
 //Page Slug Body Class
