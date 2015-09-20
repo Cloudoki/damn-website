77bffa71d390228c862e1cd86bf751a244a8acde
Michael Lythcott <mike@reccenter.com>
2015-09-20 16:29:40 +0100

    make sure typekit is active

--- web/app/themes/damn-sage/templates/head.php
+++ web/app/themes/damn-sage/templates/head.php
@@ -8,10 +8,9 @@
 
   <?php wp_head(); ?>
 
-  <!-- fonts / typekit
+  <!-- fonts / typekit -->
   <script src="https://use.typekit.net/iir2zjt.js"></script>
   <script>try{Typekit.load({ async: true });}catch(e){}</script>
-  -->
 
 </head>
 
