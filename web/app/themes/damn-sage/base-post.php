8ea05052c8957e312c3ca6d44666ac3b2b403282
ErikRFerreira <erik.rocha.ferreira@gmail.com>
2016-03-14 14:42:18 +0000

    make sure an issue appears in post sidebar

--- web/app/themes/damn-sage/base-post.php
+++ web/app/themes/damn-sage/base-post.php
@@ -60,9 +60,19 @@ $parameters->template = $template_path . (
 
 
 # Ad
-if ( function_exists ('adrotate_group')){
+if ( !$DAMN->issue->brand && function_exists ('adrotate_group')){
 	$parameters->advert = adrotate_group (3);
 }
+
+
+
+
+# Make something happen here..
+if( !$DAMN->issue ){
+	@$parameters->issue->link = get_site_url() . '/editions/' . $DAMN->issued->slug;
+	@$parameters->issue->thumbnail =  wp_get_attachment_url( get_field( 'magazine_taxonomy_image', 'magazine_' . $DAMN->issued->term_id ) );
+}
+
 
 
 
