533b465e04090a01e608f8681ee31cc478549a55
ErikRFerreira <erik.rocha.ferreira@gmail.com>
2016-04-18 15:22:39 +0100

    solution for side menu

--- web/app/themes/damn-sage/assets/scripts/general.js
+++ web/app/themes/damn-sage/assets/scripts/general.js
@@ -1,13 +1,39 @@
 jQuery(document).ready(function($) {
 
+	$('.offside-users-alphabet-menu ul li a').hide();
 
 	$('.offside-users-alphabet-menu ul li a').click(function(event) {
+
 		event.preventDefault();
 		var target = $(this).attr('href');
 
 		$('html,body').animate( {scrollTop: $(target).offset().top - 92 }, 'slow' );
 
 	});
+
+
+	$('.offside-users-alphabet-menu ul li').each(function(index, el) {
+		$(this).hover(function() {
+			$(this).find('a').show();
+			$(this).stop().animate( {
+				'width': '55px',
+				'height': '30px'
+			}, 'fast');
+			/* Stuff to do when the mouse enters the element */
+		
+		}, function() {
+			
+			$(this).stop().animate( {
+				'width': '25px',
+				'height': '15px'
+			}, 'fast');
+			$(this).find('a').hide();
+			/* Stuff to do when the mouse leaves the element */
+		});
+
+
+	});
+
 
 
 });
--- web/app/themes/damn-sage/page-contributors.php
+++ web/app/themes/damn-sage/page-contributors.php
@@ -37,7 +37,7 @@ Template Name: Contributors
 
   ?>
 
-<!--
+
 <div class="offside-users-alphabet-menu">
   <ul>
     <?php 
@@ -49,7 +49,7 @@ Template Name: Contributors
      ?>
   </ul>
 </div>
--->
+
 
   <footer class="contributors-list">
     <?php foreach($users as $user) { 
