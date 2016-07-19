4a282e061e0b1eca36cae893546eb2143fc09335
ErikRFerreira <erik.rocha.ferreira@gmail.com>
2016-07-19 10:18:41 +0100

    image size in slider

--- web/app/themes/damn-sage/templates/home-primary-row.php
+++ web/app/themes/damn-sage/templates/home-primary-row.php
@@ -31,7 +31,7 @@ foreach(get_the_category() as $category)
 
 ?>
 
-<div class="item col-xs-12 col-sm-12 slider-content <?=has_post_format('quote')? 'quote-format ':null?><?=implode (' ', $cats)?>"<?=isset ($url)? ' style="width:100%!important;height:89%!important;background-image:url(' . $url . ');"': null?>>
+<div class="item col-xs-12 col-sm-12 slider-content <?=has_post_format('quote')? 'quote-format ':null?><?=implode (' ', $cats)?>"<?=isset ($url)? ' style="width:100%!important;height:89%!important;background-image:url(' . $url . ');background-repeat:no-repeat;background-size:cover;background-position:left bottom;"': null?>>
 
 	<?php get_template_part('templates/snippet', has_post_format('quote')? 'item-post-quote': 'item-post'); ?>
 
