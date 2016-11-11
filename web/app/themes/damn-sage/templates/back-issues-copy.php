259f30555ee3ec2dd8c693c9744faa5a5b06e158
Koen Betsens <koen@cloudoki.com>
2016-11-11 10:53:00 +0100

    back-issues update

--- web/app/themes/damn-sage/templates/back-issues-copy.php
+++ web/app/themes/damn-sage/templates/back-issues-copy.php
@@ -1,6 +1,6 @@
 <div class="back-issues-copy">
 	<h4 class="text-center">SUBSCRIBE FOR JUST € 5 / MONTH</h4>
-	<h4 class="text-center">Get a YEAR deal, you pay only 10 AND GET TWO MONTHS ON THE HOUSE.</h4>
+	<h4 class="text-center"><small>our</small>​ ONE YEAR deal – pay for 10 <small>issues</small> and get 2 <small>​more</small> on the house​!​</h4>
 	<br/>
 	<h4 class="subscribe-link">
 		<a href="https://www.bruil.info/product/damn/">Subscribe</a>
